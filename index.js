2ea762101519bb52e6e496887106618889a43fe2
Bryce Dyck <bryce.dyck@alumni.ubc.ca>
2024-02-18 09:29:26 -0800

    Basic app shell

---
+++ index.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-template-curly-in-string */
+const express = require('express');
+const app = express();
+const port = 8888;
+
+app.get('/', (req, res) => {
+    const data = {
+        name: 'Bryce',
+        isAwesome: false
+    };
+
+    res.json(data);
+});
+
+app.get("/awesome-generator", (req, res) => {
+    const {name, isAwesome} = req.query;
+    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
+})
+
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`)
+})
