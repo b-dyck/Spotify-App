a8ed7dc0e3a1c6c7b54b18e3085732344529597a
Bryce Dyck <bryce.dyck@alumni.ubc.ca>
2024-02-19 14:20:41 -0800

    Implement step 4 of OAuth flow by receiving access token from spotify. Utilize axios to do so

--- index.js
+++ index.js
@@ -1,25 +1,77 @@
+/* eslint-disable new-cap */
 /* eslint-disable no-template-curly-in-string */
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const axios = require('axios')
 const port = 8888;
+const querystring = require('querystring');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-app.get('/', (req, res) => {
-    const data = {
-        name: 'Bryce',
-        isAwesome: false
-    };
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+  
+  
+  const stateKey = 'spotify_auth_state';
+  
+  app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+  
+    const scope = 'user-read-private user-read-email';
+  
+    const queryParams = querystring.stringify({
+      client_id: CLIENT_ID,
+      response_type: 'code',
+      redirect_uri: REDIRECT_URI,
+      state: state,
+      scope: scope,
+    });
+  
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+  })
 
-    res.json(data);
-});
-
-app.get('/login', (req, res) => {
-    res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
-})
+  app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+  
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+      .then(response => {
+        if (response.status === 200) {
+          res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        } else {
+          res.send(response);
+        }
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  });
 
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`)
