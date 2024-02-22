c47721a1d6ed1784014e98b1cd6572d501058fd2
Bryce Dyck <bryce.dyck@alumni.ubc.ca>
2024-02-22 14:50:01 -0800

    Optimize React and Express with npm concurrently

--- index.js
+++ index.js
@@ -65,28 +65,49 @@ const generateRandomString = length => {
     .then(response => {
         if (response.status === 200) {
     
-          const { access_token, token_type } = response.data;
-    
-          axios.get('https://api.spotify.com/v1/me', {
-            headers: {
-              Authorization: `${token_type} ${access_token}`
-            }
+          const { access_token, refresh_token } = response.data;
+          const queryParams = querystring.stringify({
+            access_token,
+            refresh_token
           })
-            .then(response => {
-              res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-            })
-            .catch(error => {
-              res.send(error);
-            });
+ 
+          // redirect to react app
+          res.redirect(`http://localhost:3000/?${queryParams}`)
+          // pass along tokens in query params
     
         } else {
-          res.send(response);
+          res.redirect(`/?${querystring.stringify({
+            error: 'invalid_token'
+          })}`);
         }
       })
       .catch(error => {
         res.send(error);
       });
   })
+
+  app.get('/refresh_token', (req, res) => {
+    const { refresh_token } = req.query;
+  
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  });
 
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`)
