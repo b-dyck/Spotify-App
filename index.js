a790d9f2d06601ee639fb8fc45b62b4180d7e2e4
Bryce Dyck <bryce.dyck@alumni.ubc.ca>
2024-02-23 08:41:16 -0800

    Create spotify.js to handle query param logic. Refactor app.js to import access token from spotify.js, use useState hook to keep track of token. conditionally render log in button

--- index.js
+++ index.js
@@ -65,10 +65,11 @@ const generateRandomString = length => {
     .then(response => {
         if (response.status === 200) {
     
-          const { access_token, refresh_token } = response.data;
+          const { access_token, refresh_token, expires_in } = response.data;
           const queryParams = querystring.stringify({
             access_token,
-            refresh_token
+            refresh_token,
+            expires_in
           })
  
           // redirect to react app
