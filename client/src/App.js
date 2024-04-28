4bacf74e457d6781a2f1ab0f2ec044db7cadb838
Bryce Dyck <bryce@dhcp-128-189-148-230.ubcsecure.wireless.ubc.ca>
2024-04-28 16:08:13 -0700

    Create route scaffolding with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -32,18 +36,28 @@ function App() {
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <button onClick={logout}>Log Out</button>
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar"/>
-                )}
-              </div>
-            )}
-          </>
+          <Router>
+            <Routes>
+              <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+              <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+              <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+              <Route path="/playlists" element={<h1>Playlists</h1>} />
+              <Route path="/" element={
+                <>
+                  <button onClick={logout}>Log Out</button>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              } />
+            </Routes>
+          </Router>
         )}
       </header>
     </div>
